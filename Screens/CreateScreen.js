4bdbe17b64dd95ec9f3329780216953d31e0f59c
Mariia Makarkina <mariiamak25@gmail.com>
2023-04-28 02:59:28 +0300

    fix camera

--- Screens/CreateScreen.js
+++ Screens/CreateScreen.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+// import { View, Text, StyleSheet, Image } from "react-native";
 import { Camera } from "expo-camera";
-import { TouchableOpacity } from "react-native-gesture-handler";
+// import { TouchableOpacity } from "react-native-gesture-handler";
 import * as Location from "expo-location";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image
+} from "react-native";
 
 const CreateScreen = ({ navigation }) => {
   const [camera, setCamera] = useState(null);
-  const [photo, setPhoto] = useState('');
+  const [photo, setPhoto] = useState(null);
 
   const takePhoto = async () => {
     const photo = await camera.takePictureAsync();
-    const location = await Location.getCurrentPositionAsync();
-    console.log("latitude", location.coords.latitude);
-    console.log("longitude", location.coords.longitude);
+    // const location = await Location.getCurrentPositionAsync();
+    // console.log("latitude", location.coords.latitude);
+    // console.log("longitude", location.coords.longitude);
     setPhoto(photo.uri);
     console.log("photo", photo);
     console.log("camera", camera)
