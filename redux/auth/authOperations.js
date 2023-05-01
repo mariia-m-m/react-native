64dff384a4d182950f3b7151572bf0fd9d2e1f25
Mariia Makarkina <mariiamak25@gmail.com>
2023-05-02 01:24:33 +0300

    add auth

---
+++ redux/auth/authOperations.js
@@ -0,0 +1,3 @@
+const authSignInUser = () => useSyncExternalStore(dispatch, getState)=> { };
+const authSignUpUser = () => useSyncExternalStore(dispatch, getState)=> { };
+const authSignOutUser = () => useSyncExternalStore(dispatch, getState)=> { };
