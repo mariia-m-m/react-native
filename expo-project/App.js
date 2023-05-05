af564b840ebf8cfe062f5f7ff8280a45a465ffa3
Mariia Makarkina <mariiamak25@gmail.com>
2023-05-05 15:16:30 +0300

    add screens

--- expo-project/App.js
+++ expo-project/App.js
@@ -1,20 +1,35 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { useRoute } from "./router";
+import { Provider } from '@reduxjs/toolkit';
+import {store} from "./redux/store"
+
+
+
+// const loadApplication = async () => {
+//   await Font.loadAsync({
+//     "DMMono-Regular": require("./assets/fonts/DMMono-Regular.ttf"),
+//   });
+// };
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [iasReady, setIasReady] = useState(false);
+  const routing = useRoute(true);
+  // if (!iasReady) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={loadApplication}
+  //       onFinish={() => setIasReady(true)}
+  //       onError={console.warn}
+  //     />
+  //   );
+  // }
+
+  return <Provider store = {store}><NavigationContainer>{routing}</NavigationContainer></Provider>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
